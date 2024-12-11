3ef5796af94e9c8906518b0aff3d89ba9acb1de0
Tomas Morales <tomasmorales004@gmail.com>
2024-12-11 19:47:57 -0300

    Agregue los superusers a los empleados para que tengan acceso a solo lo que pueden acceder los empleados.

--- empleado/models.py
+++ empleado/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 from sucursal.models import Sucursal
 
-
 class Empleado(models.Model):
+    # Relación OneToOne con User (esto permite asociar un usuario de Django a un empleado)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='empleado', null=True, blank=True)
+    
     sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
-    nombre= models.CharField(max_length=20, unique=True)
-    apellido= models.CharField(max_length=50)
+    nombre = models.CharField(max_length=20, unique=True)
+    apellido = models.CharField(max_length=50)
     DNI = models.CharField(max_length=8)
     fecha_contratacion = models.DateField()
 
