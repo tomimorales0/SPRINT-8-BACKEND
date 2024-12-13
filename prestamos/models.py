6dcf03145671b82e6957385fd85f45dbabe3c3ee
Tomas Morales <tomasmorales004@gmail.com>
2024-12-13 20:26:40 -0300

    Version 1.0 Sprint 8-Backend

--- prestamos/models.py
+++ prestamos/models.py
@@ -6,7 +6,7 @@ class Prestamo(models.Model):
     tipo_prestamo = models.CharField(max_length=100, null=False, default= 'Classic')
     fecha_inicio = models.DateField()
     monto = models.DecimalField(max_digits=10, decimal_places=2)
-    aprobado = models.BooleanField(default=False)
+    aprobado = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Préstamo de {self.cliente.user.username} - {self.monto}"
